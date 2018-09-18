f493fbf9951f3105feb20281a0b836955850acbd
T <shrmrf@users.noreply.github.com>
2018-09-18 21:47:27 +0200

    returning a correct string

--- src/coinlib.rs
+++ src/coinlib.rs
@@ -3,53 +3,6 @@ extern crate serde_json;
 use std;
 use std::error::Error;
 
-// #[cfg(test)]
-// #[test]
-// fn it_works() {
-//     let coinlib_handler = Coinlib::new("69fd5168e0847c19", "BTC", "USD");
-//     let request_url = format!(
-//         "https://coinlib.io/api/v1/coin?key={key}&pref=USD&symbol={ticker}",
-//         key = "69fd5168e0847c19",
-//         ticker = "BTC"
-//     );
-
-//     let client;
-//     match std::env::vars().find(|(name, value)| name == &"http_proxy".to_string()) {
-//         None => client = reqwest::Client::new(),
-
-//         // Todo: improve this check
-//         Some((_name, value)) => {
-//             println!("{}", value);
-//             let url = reqwest::Proxy::all(&value).unwrap();
-//             println!("{:?}", value);
-//             // Todo: fix the &value --> url conversion
-//             client = reqwest::Client::builder().proxy(url).build().unwrap()
-//         }
-//     }
-//     let mut response = client.get(&request_url).send().unwrap(); //reqwest::get(&request_url)?;
-
-//     // if !response.status().is_success() {
-//     //     Err(format!("{}", response.status()));
-//     // }
-
-//     let coin_info = response.text().unwrap();
-//     //println!("{}", coin_info);
-
-//     use serde_json::{Error, Value};
-//     let v: Value = serde_json::from_str(&coin_info).unwrap();
-
-//     println!(
-//         r#"BTC:
-//             {}USD
-//             rank: {}
-//             %change (7d): {}%
-//         "#,
-//         v["price"], v["rank"], v["delta_7d"]
-//     );
-
-//     assert_eq!(2 + 2, 4);
-// }
-
 #[test]
 fn auth_init_test() {
     let auth = CoinlibAuth::new("abcdef");
