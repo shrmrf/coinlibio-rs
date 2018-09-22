5eb51e189636243426a717326d15d2ec2f740d7a
T <shrmrf@users.noreply.github.com>
2018-09-22 22:45:06 +0200

    remove misplaced integ test

--- src/coinlib.rs
+++ src/coinlib.rs
@@ -61,28 +61,6 @@ impl CoinlibAuth {
     }
 }
 
-#[test]
-fn test_api_string_creation() {
-    match std::env::vars().find(|(name, value)| name == &"COINLIB_TOKEN".to_string()) {
-        None => panic!("export COINLIB_TOKEN=<token>"),
-        Some((_name, value)) => {
-            let auth = CoinlibAuth::new(&value);
-            let api = CoinlibApi::new(&auth.unwrap()).unwrap();
-
-            println!(
-                "{}",
-                api.request(
-                    Endpoint::Coin,
-                    EndpointParams {
-                        currency: "USD".to_string(),
-                        symbol: "BTC".to_string()
-                    }
-                ).unwrap()
-            );
-        }
-    }
-}
-
 pub struct CoinlibApi {
     api_key: String,
 }
